56df5681a452d0e50ab72073a336bff7658c4d58
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-02-05 15:04:57 +0100

    Fix moving bug

--- SnakeGameConsole/SnakeGame/Game.cs
+++ SnakeGameConsole/SnakeGame/Game.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 
 namespace SnakeGame
 {
-    class Game
+    internal class Game
     {
         public int Height { get; private set; }
         public int Width { get; private set; }
         public string[,] Map { get; private set; }
         public Snake Snake { get; private set; }
 
-        public Game() : this(20,30)
+        public Game() : this(20, 30)
         {
         }
+
         public Game(int h, int w)
         {
             this.Height = h;
@@ -22,20 +21,20 @@ namespace SnakeGame
             this.Snake = new Snake(new List<(int, int)>() { (3, 3), (3, 2), (3, 1) }, Direction.Right);
             this.Render();
             this.Play();
-            
         }
+
         private void Play()
         {
             ConsoleKeyInfo key;
             do
             {
                 key = Console.ReadKey(true);
-                while(!isValidKey(key))
+                while (!isValidKey(key))
                 {
                     key = Console.ReadKey(true);
                 }
 
-                if(key.Key != ConsoleKey.Escape)
+                if (key.Key != ConsoleKey.Escape)
                 {
                     var direction = GetDirectionFromKey(key.Key);
                     this.Move(direction);
@@ -45,15 +44,26 @@ namespace SnakeGame
             } while (key.Key != ConsoleKey.Escape);
         }
 
+        /// <summary>
+        /// Check if the input key is allowed
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         private bool isValidKey(ConsoleKeyInfo key)
         {
-            var validKeys = new List<ConsoleKey>() {ConsoleKey.Enter,ConsoleKey.W,ConsoleKey.A, ConsoleKey.D,ConsoleKey.S,ConsoleKey.Escape};
-            if(validKeys.Contains(key.Key))
+            var validKeys = new List<ConsoleKey>() { ConsoleKey.Enter, ConsoleKey.W, ConsoleKey.A, ConsoleKey.D, ConsoleKey.S, ConsoleKey.Escape };
+            if (validKeys.Contains(key.Key))
             {
                 return true;
             }
             return false;
         }
+
+        /// <summary>
+        /// Get direction from input key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         private Direction GetDirectionFromKey(ConsoleKey key)
         {
             Direction direction;
@@ -63,85 +73,105 @@ namespace SnakeGame
                     direction = this.Snake.Direction;
                     break;
                 case ConsoleKey.A:
-                    if(this.Snake.Direction == Direction.Right)
-                    {
-                        direction = this.Snake.Direction;                        
-                    }
-                    else
-                    {
-                        direction = Direction.Left;
-                    }                    
+                    direction = Direction.Left;
                     break;
                 case ConsoleKey.D:
-                    if (this.Snake.Direction == Direction.Left)
-                    {
-                        direction = this.Snake.Direction;
-                    }
-                    else
-                    {
-                        direction = Direction.Right;
-                    }                    
+                    direction = Direction.Right;
                     break;
                 case ConsoleKey.S:
-                    if (this.Snake.Direction == Direction.Up)
-                    {
-                        direction = this.Snake.Direction;
-                    }
-                    else
-                    {
-                        direction = Direction.Down;
-                    }                    
+                    direction = Direction.Down;
                     break;
                 case ConsoleKey.W:
-                    if (this.Snake.Direction == Direction.Down)
-                    {
-                        direction = this.Snake.Direction;
-                    }
-                    else
-                    {
-                        direction = Direction.Up;
-                    }                    
+                    direction = Direction.Up;
                     break;
                 default:
                     direction = this.Snake.Direction;
                     break;
             }
-            return direction;            
+            return direction;
         }
+
+        /// <summary>
+        /// Move the snake around
+        /// </summary>
+        /// <param name="direction"></param>
         private void Move(Direction direction)
         {
             (int, int) currentPos = this.Snake.Head;
             
+            if(!isPossibleToMove(direction))
+            {
+                return;
+            }            
+
             switch (direction)
             {
-                case Direction.Up:                    
-                    (int,int)nextPos = (currentPos.Item1 + 1, currentPos.Item2);
+                case Direction.Up:
+                    (int, int) nextPos = (currentPos.Item1 - 1, currentPos.Item2);
                     this.Snake.Move(nextPos);
+                    this.Snake.Direction = Direction.Up;
                     break;
+
                 case Direction.Down:
-                    nextPos = (currentPos.Item1 - 1, currentPos.Item2);
+                    nextPos = (currentPos.Item1 + 1, currentPos.Item2);
                     this.Snake.Move(nextPos);
+                    this.Snake.Direction = Direction.Down;
                     break;
+
                 case Direction.Left:
-                    nextPos = (currentPos.Item1, currentPos.Item2-1);
+                    nextPos = (currentPos.Item1, currentPos.Item2 - 1);
                     this.Snake.Move(nextPos);
+                    this.Snake.Direction = Direction.Left;
                     break;
+
                 case Direction.Right:
-                    nextPos = (currentPos.Item1, currentPos.Item2+1);
+                    nextPos = (currentPos.Item1, currentPos.Item2 + 1);
                     this.Snake.Move(nextPos);
+                    this.Snake.Direction = Direction.Right;
                     break;
+
                 default:
                     break;
             }
-
         }
+        /// <summary>
+        /// Check if next move is possible
+        /// </summary>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        private bool isPossibleToMove(Direction direction)
+        {
+            var currentDirection = this.Snake.Direction;
+
+            if (currentDirection == Direction.Left && direction == Direction.Right)
+            {
+                return false;
+            }
+            if (currentDirection == Direction.Right && direction == Direction.Left)
+            {
+                return false;
+            }
+            if (currentDirection == Direction.Up && direction == Direction.Down)
+            {
+                return false;
+            }
+            if (currentDirection == Direction.Down && direction == Direction.Up)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Draw and print the map
+        /// </summary>
         private void Render()
         {
             Console.WriteLine($"Map: {this.Height} x {this.Width}");
             this.DrawMap(this.Snake);
             this.PrintMap();
         }
-        
+
         /// <summary>
         /// Draw the walls, fill the field with ""
         /// </summary>
@@ -151,25 +181,25 @@ namespace SnakeGame
             this.Map = new string[this.Height, this.Width];
 
             //Horizontal walls
-            for(int j =0; j< this.Width;j++)
+            for (int j = 0; j < this.Width; j++)
             {
                 this.Map[0, j] = "*";
                 this.Map[this.Height - 1, j] = "*";
             }
 
             //Vertical walls
-            for (int i =1; i< this.Height-1;i++ )
+            for (int i = 1; i < this.Height - 1; i++)
             {
                 this.Map[i, 0] = "*";
-                this.Map[i, this.Width-1] = "*";
+                this.Map[i, this.Width - 1] = "*";
             }
 
             //Field
-            for (int i = 1; i < this.Height-1; i++)
+            for (int i = 1; i < this.Height - 1; i++)
             {
-                for (int j = 1; j < this.Width-1; j++)
+                for (int j = 1; j < this.Width - 1; j++)
                 {
-                    if(snake.SnakeBody.Contains((i,j)))
+                    if (snake.SnakeBody.Contains((i, j)))
                     {
                         this.Map[i, j] = "0";
                     }
@@ -183,17 +213,17 @@ namespace SnakeGame
             //Snake head
             this.Map[snake.Head.Item1, snake.Head.Item2] = "X";
         }
+
         public void PrintMap()
         {
             for (int i = 0; i < this.Height; i++)
             {
                 for (int j = 0; j < this.Width; j++)
                 {
-                    Console.Write(this.Map[i, j].PadLeft(1)+ " ");
+                    Console.Write(this.Map[i, j].PadLeft(1) + " ");
                 }
                 Console.WriteLine();
             }
         }
-        
     }
 }
