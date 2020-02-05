29537261584cb21961ff774d3eaeb4d5165e0b07
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-02-05 12:28:45 +0100

    Add classes.

---
+++ SnakeGameConsole/SnakeGame/Apple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    class Apple
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Apple(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return String.Format($"[{X}:{Y}]");
+        }
+        
+    }
+}
